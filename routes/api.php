a244c3d6648d70082d032a0381e087c8ab4ccff6
muhammad reza alvi <mrezaalvi@gmail.com>
2024-08-09 16:56:16 +0700

    create authentication

---
+++ routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+   Route::get('/user', function (Request $request) {
+        return $request->user();
+   }); 
+
+   Route::post('logout', function(Request $request){
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Logout Success',
+        ]);
+   });
+});
+Route::post('login', function(Request $request){
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if(!$user || !Hash::check($request->password, $user->password)){
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return response()->json([
+        'token' => $user->createToken($request->device_name)->plainTextToken,
+    ]);
+
+});
